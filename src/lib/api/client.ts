aff169efd9eeeffdd6cd89a7a87e999070226433
shimeji221418 <shimeji2214@gmail.com>
2023-05-12 23:00:10 +0900

    create .env

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -20,7 +20,7 @@ const options = {
 
 const baseClient = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: `${process.env.BASE_URL}/api/v1/`,
   }),
   options
 );
