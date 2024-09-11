b551b67fe6e692c8dd43fe1a149aba2842eda364
shimeji221418 <shimeji2214@gmail.com>
2024-09-11 18:23:14 +0900

    weekly_scheduleの名前幅を統一

--- src/hooks/useGetTeamUsers.ts
+++ src/hooks/useGetTeamUsers.ts
@@ -13,28 +13,25 @@ export const useGetTeamUsers = (teamId: number) => {
   const auth = getAuth(app);
   const { loginUser } = useAuthContext();
   const [teamUser, setTeamUser] = useState<Array<GetUserType>>([]);
-  const getTeamUsers = useCallback(
-    async (props: Props) => {
-      const { team_id } = props;
-      try {
-        const token = await auth.currentUser?.getIdToken(true);
-        if (loginUser) {
-          const data = { team_id: team_id };
-          const props: BaseClientWithAuthType = {
-            method: "get",
-            url: "/users/team_users",
-            token: token!,
-            params: data,
-          };
-          const res = await BaseClientWithAuth(props);
-          setTeamUser(res.data);
-        }
-      } catch (e: any) {
-        console.log(e);
+  const getTeamUsers = useCallback(async (props: Props) => {
+    const { team_id } = props;
+    try {
+      const token = await auth.currentUser?.getIdToken(true);
+      if (loginUser) {
+        const data = { team_id: team_id };
+        const props: BaseClientWithAuthType = {
+          method: "get",
+          url: "/users/team_users",
+          token: token!,
+          params: data,
+        };
+        const res = await BaseClientWithAuth(props);
+        setTeamUser(res.data);
       }
-    },
-    [teamUser, loginUser]
-  );
+    } catch (e: any) {
+      console.log(e);
+    }
+  }, []);
 
   const GetAllUsers = useCallback(async () => {
     try {
@@ -45,12 +42,11 @@ export const useGetTeamUsers = (teamId: number) => {
         token: token!,
       };
       const res = await BaseClientWithAuth(props);
-      console.log(res.data);
       setTeamUser(res.data);
     } catch (e: any) {
       console.log(e);
     }
-  }, [teamUser]);
+  }, []);
 
   useEffect(() => {
     if (loginUser && teamId != 0) {
@@ -59,6 +55,6 @@ export const useGetTeamUsers = (teamId: number) => {
     if (loginUser && teamId == 1) {
       GetAllUsers();
     }
-  }, [loginUser, teamId]);
+  }, [teamId]);
   return { getTeamUsers, teamUser, setTeamUser };
 };
